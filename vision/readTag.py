3992da218bfe3fe1b11385a5dd21580c38bfef08
Yuening Zhang <zhangyn@umich.edu>
2015-10-29 02:50:09 +0000

    add testing files

--- vision/readTag.py
+++ vision/readTag.py
@@ -1,43 +1,37 @@
-# USAGE
-# python track.py --video video/iphonecase.mov
-
 # import the necessary packages
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 import numpy as np
-import argparse
 import time
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help = "path to the (optional) video file")
-args = vars(ap.parse_args())
-
-# define the upper and lower boundaries for a color
-# to be considered "blue"
+# define macros
 blueLower = np.array([0, 0, 0], dtype = "uint8")
-blueUpper = np.array([100, 100, 100], dtype = "uint8")
+blueUpper = np.array([60, 60, 60], dtype = "uint8")
 orientation_flag = [[1,1,0,0],
 					[0,1,1,0],
 					[0,0,1,1],
 					[1,0,0,1]]
+car_center = (640/2,480/2)
+error_check_value_1 = 39
+error_check_value_2 = 18
+threshold_value = 60
 
 # load the video
-camera = cv2.VideoCapture(args["video"])
-(grabbed, frame) = camera.read()
-car_center = (frame.shape[0]/2,frame.shape[1]/2)
-# print video_center
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera,size = (640,480))
 
-# keep looping
-while True:
-	# grab the current frame
-	(grabbed, frame) = camera.read()
+# warm up camera
+time.sleep(0.1)
 
-	# check to see if we have reached the end of the
-	# video
-	if not grabbed:
-		break
+# capture frames from stream
+for image_raw in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+	frame = image_raw.array
+	# cv2.imshow("Frame",frame)
 
+	
 	# determine which pixels fall within the blue boundaries
 	# and then blur the binary image
 	blue = cv2.inRange(frame, blueLower, blueUpper)
@@ -49,9 +43,7 @@ while True:
 
 	# check to see if any contours were found
 	if len(cnts) > 0:
-		# sort the contours and find the largest one -- we
-		# will assume this contour correspondes to the area
-		# of my phone
+		# sort the contours and find the largest one
 		cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 
 		# compute the (rotated) bounding box around then
@@ -69,16 +61,17 @@ while True:
 		# contour and then draw it		
 		cv2.drawContours(frame, [rect], -1, (0, 255, 0), 2)
 		pts = np.float32([[100,100],[0,100],[0,0],[100,0]])
+		# Perspective transform
 		M = cv2.getPerspectiveTransform(np.float32(rect),pts)
 		p_image = cv2.warpPerspective(frame,M,(100,100))
+		# Gray image
 		gray = cv2.cvtColor(p_image, cv2.COLOR_BGR2GRAY)
-		# cv2.circle(frame, (rect[0][0],rect[0][1]), 5, (0, 255, 0), -1)
-		# cv2.circle(frame, (rect[1][0],rect[1][1]), 5, (255, 0, 0), -1)
-		# cv2.circle(frame, (rect[2][0],rect[2][1]), 5, (0, 0, 255), -1)
-		# cv2.circle(frame, (rect[3][0],rect[3][1]), 5, (255, 255, 0), -1)
-		(T, threshInv) = cv2.threshold(gray, 155, 255, cv2.THRESH_BINARY)
+		# Take threshold
+		(T, threshInv) = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+		# Resize to 8*8
 		rs_image = cv2.resize(threshInv, (8, 8), interpolation = cv2.INTER_AREA)
-		(T, thresh) = cv2.threshold(rs_image, 155, 1, cv2.THRESH_BINARY)
+		# Take threshold
+		(T, thresh) = cv2.threshold(rs_image, threshold_value, 1, cv2.THRESH_BINARY)
 		point_value = [thresh[1][1],thresh[1][6],thresh[6][6],thresh[6][1]]
 		# print angle
 		if point_value == orientation_flag[0]:
@@ -105,18 +98,26 @@ while True:
 			threshInv = cv2.transpose(threshInv)
 			threshInv = cv2.flip(threshInv,flipCode = 1)
 			angle = angle + 90
-		print angle
+		# print angle
 
 	# show the frame and the binary image
-	# print thresh
+	print thresh
 	value = 0
+	value_2 = 0
 	for x in xrange(6):
 		value = value*2 + thresh[1][x+1]
-	# print value
+		value_2 = value_2*2 + thresh[6][x+1]
+	if value != error_check_value_1	or value_2 != error_check_value_2:
+		print "Tag not recognized."
+	else:
+		value = 0
+		for x in xrange(6):
+			value = value*2 + thresh[2][x+1]
+		print value
 	# cv2.imshow("threshold",threshInv)
 	cv2.imshow("Tracking", frame)
 	#cv2.imshow("Binary", blue)
-	# cv2.imshow("Cropped",p_image)
+	cv2.imshow("Cropped",p_image)
 	# cv2.waitKey(0)
 
 	# if your machine is fast, it may display the frames in
@@ -126,11 +127,8 @@ while True:
 	# however, if your computer is slow, you probably want to
 	# comment out this line
 	#time.sleep(0.025)
-
+	
+	rawCapture.truncate(0)
 	# if the 'q' key is pressed, stop the loop
 	if cv2.waitKey(1) & 0xFF == ord("q"):
 		break
-
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
